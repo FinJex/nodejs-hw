587af530616e9e2de6e4122e8b906d68940788b4
FinJex <maks.vasylovych12@gmail.com>
2026-07-14 22:07:45 +0200

    hw3

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -1,46 +1,42 @@
 import { Joi, Segments } from 'celebrate';
 import { isValidObjectId } from 'mongoose';
-const tagsList = require('../constants/tags.js');
+import tagsList from '../constants/tags.js';
 
-//allNotes
-export const getAllNotesSchema = {
-[Segments.BODY]: Joi.object({
-page: Joi.number().integer().min(1).default(1),
-perPage: Joi.number().integer().min(5).max(20).default(10),
-tag: Joi.string().valid(...tagsList).optional(),
-search: Joi.string().optional(),
-})
-};
-
-
-//NotesId
 const objectIdValidator = (value, helpers) => {
   return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
+};
+
+export const getAllNotesSchema = {
+  [Segments.QUERY]: Joi.object({
+    page: Joi.number().integer().min(1).default(1),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
+    tag: Joi.string().valid(...tagsList).allow('').optional(),
+    search: Joi.string().allow('').optional(),
+  })
 };
 
 export const noteIdSchema = {
   [Segments.PARAMS]: Joi.object({
     noteId: Joi.string().custom(objectIdValidator).required(),
-})};
-
-//Post
-export const createNoteSchema = {
-[Segments.PARAMS]: Joi.object({
-  title: Joi.string().min(1).required(),
-  content: Joi.string().optional(),
-  tag: Joi.valid(...tagsList).optional(),
- })
+  })
 };
 
-//Patch
+export const createNoteSchema = {
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).required(),
+    content: Joi.string().allow('').optional(),
+    tag: Joi.string().valid(...tagsList).allow('').optional(),
+  })
+};
+
 export const updateNoteSchema = {
   [Segments.PARAMS]: Joi.object({
     noteId: Joi.string().custom(objectIdValidator).required(),
   }),
   [Segments.BODY]: Joi.object({
-  title: Joi.string().min(1).optional(),
-  content: Joi.string().optional(),
-  tag: Joi.valid(...tagsList).optional(),
+    title: Joi.string().min(1).optional(),
+    content: Joi.string().allow('').optional(),
+    tag: Joi.string().valid(...tagsList).allow('').optional(),
   }).min(1),
 };
 
