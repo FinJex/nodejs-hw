db3c27c1e9990cec9784fba2afac62ef2dfa570f
FinJex <maks.vasylovych12@gmail.com>
2026-07-14 22:12:10 +0200

    hw3

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -1,6 +1,6 @@
 import { Joi, Segments } from 'celebrate';
 import { isValidObjectId } from 'mongoose';
-import tagsList from '../constants/tags.js';
+import { TAGS } from '../constants/tags.js';
 
 const objectIdValidator = (value, helpers) => {
   return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
@@ -10,7 +10,7 @@ export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
-    tag: Joi.string().valid(...tagsList).allow('').optional(),
+    tag: Joi.string().valid(...TAGS).allow('').optional(),
     search: Joi.string().allow('').optional(),
   })
 };
@@ -25,7 +25,7 @@ export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).required(),
     content: Joi.string().allow('').optional(),
-    tag: Joi.string().valid(...tagsList).allow('').optional(),
+    tag: Joi.string().valid(...TAGS).allow('').optional(),
   })
 };
 
@@ -36,7 +36,7 @@ export const updateNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).optional(),
     content: Joi.string().allow('').optional(),
-    tag: Joi.string().valid(...tagsList).allow('').optional(),
+    tag: Joi.string().valid(...TAGS).allow('').optional(),
   }).min(1),
 };
 
