38be92e8d5612aa00f80d6f32beed708d63666ce
FinJex <maks.vasylovych12@gmail.com>
2026-07-27 16:06:12 +0200

    hw 5

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -13,7 +13,11 @@ import {
   loginUserSchema,
 } from "../validations/authValidation.js";
 
+import { resetPassword } from '../controllers/authController.js';
+import { resetPasswordSchema } from '../validations/authValidation.js';
 
+import { requestResetEmail } from '../controllers/authController.js';
+import { requestResetEmailSchema } from '../validations/authValidation.js';
 const router = Router();
 
 router.post("/auth/register", celebrate(registerUserSchema), registerUser);
@@ -21,6 +25,7 @@ router.post("/auth/login", celebrate(loginUserSchema), loginUser);
 router.post("/auth/refresh", refreshUserSession);
 router.post("/auth/logout", logoutUser);
 
-
+router.post('/auth/reset-password', celebrate(resetPasswordSchema), resetPassword);
+router.post("/auth/request-reset-email", celebrate(requestResetEmailSchema), requestResetEmail);
 
 export default router;
